2ecd999b6cd86136b765ef9083d55f6965b944c2
adexphem <adexphem4real@gmail.com>
2020-02-16 17:40:32 +0100

    basic data types in typescripts

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('Invalid Input type');
   // }
-  return n1 + n2;
+
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(`${phrase} ${result}`)
+  } else {
+    return result; 
+  }
 }
 
 const number1 = 5.3;
 const number2 = 4;
+const printResult = true;
+const resultPhrase = 'Result is ';
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
+// console.log(result);
